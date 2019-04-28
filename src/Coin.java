2a89c6038351cae333abaef14bd239e6e38d2814
ZappaBoy <45740621+ZappaBoy@users.noreply.github.com>
2019-04-28 19:29:06 +0200

    Init
    intialization

---
+++ src/Coin.java
@@ -0,0 +1,10 @@
+public class Coin {
+
+    public String name;
+    public float price;
+    public float marketCap;
+    public float dailyPriceMax;
+    public float dailyPriceMin;
+    public float dailyChange;
+
+}
